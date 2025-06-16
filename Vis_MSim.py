f42a14ae050e9746262a3cb158f130dcaada6661
BasDr <basdrost2000@gmail.com>
2025-06-16 12:23:34 +0200

    Update Vis_MSim.py

--- Vis_MSim.py
+++ Vis_MSim.py
@@ -12,7 +12,7 @@ from openpyxl.styles import Font, Alignment, Border, Side, PatternFill
 import matplotlib.colors as mcolors
 from mpl_toolkits.mplot3d import Axes3D
 
-os.chdir('/vol/specs07/BasDrost/SL_simulation/')
+os.chdir('Location of folder')
 
 # Define the grid points
 gc = np.logspace(np.log10(1), np.log10(70), 40)  # Logarithmic Global Coupling
@@ -553,7 +553,7 @@ def top_10_mean_corr():
         sheet.column_dimensions[col_letter].width = max_length + 2
 
     # Save the workbook
-    workbook.save("/vol/specs07/BasDrost/SL_simulation/graphs_mean/1_to_70/Mean_analyses_formatted_error.xlsx")
+    workbook.save("graphs_mean/1_to_70/Mean_analyses_formatted_error.xlsx")
 
 def top_10_mean_corr_all():
     # Create a workbook and worksheet
@@ -719,7 +719,7 @@ def top_10_mean_corr_all():
         sheet.column_dimensions[col_letter].width = max_length + 2
 
     # Save the workbook
-    workbook.save("/vol/specs07/BasDrost/SL_simulation/graphs_mean/1_to_70/Mean_analyses_formatted_XXL.xlsx")
+    workbook.save("graphs_mean/1_to_70/Mean_analyses_formatted_XXL.xlsx")
 
 def plot_psd_parameter_relation(contour_lines=bool):
     a = 0
@@ -768,242 +768,6 @@ def plot_psd_parameter_relation(contour_lines=bool):
         plt.savefig('graphs_mean/1_to_70/PSD_Peak_color_contour.png')
     else:
         plt.savefig('graphs_mean/1_to_70/PSD_Peak_color.png')
-
-def plot_psd_parameter_relation_dif_color():
-    a = 0
-    plt.figure(figsize=(simulation_only_x, simulation_only_y))
-    
-    for a_val in range(len(a_range)):
-        plt.subplot(1, amount_of_a, a+1)
-        base_colors = plt.colormaps.get_cmap('tab20').colors  # Get 20 distinct colors
-        color_list = list(base_colors) * 2  # Repeat them to get 40 colors
-        np.random.seed(20)
-        np.random.shuffle(color_list)
-        # Step 2: Manually reassign colors to avoid direct neighbors sharing colors
-        final_colors = [None] * 40
-        final_colors[0] = color_list[0]
-
-        for i in range(1, 40):
-            # Ensure the color is not the same as the previous one
-            possible_colors = [c for c in color_list if not np.array_equal(c, final_colors[i - 1])]
-            final_colors[i] = possible_colors[0]  # Pick the first valid one
-            for idx, c in enumerate(color_list):
-                if np.array_equal(c, possible_colors[0]):
-                    del color_list[idx]  # Remove first occurrence
-                    break  # Stop after removing one
-
-        # Step 3: Create a colormap from these 40 assigned colors
-        cmap = mcolors.ListedColormap(final_colors)
-        #norm = mcolors.BoundaryNorm(boundaries=np.arange(41)-0.5, ncolors=40)
-        # Plot Mean Correlation for Youth
-        plot_list = np.nan * np.zeros((len(gc), len(md)))
-        
-        for r in range(43):
-            for i in range(len(gc)):
-                for j in range(len(md)):
-                    if r-0.5 <= all_psd_peak[a_val][i][j] < r+0.5:
-                        plot_list[i][j] = r
-        
-        plt.imshow(plot_list, aspect='auto', origin='lower', cmap=cmap, interpolation='nearest'
-                )
-        plt.colorbar()
-
-        # Add contour lines at each integer level to show clear separations
-        #plt.contour(plot_list, levels=np.arange(0, 41, 1), colors='black', linewidths=0.5)
-
-        for i in range(plot_list.shape[0]):  # Iterate over rows
-            for j in range(plot_list.shape[1]):  # Iterate over columns
-                value = int(plot_list[i, j])  # Get the value at this position
-                #text_color = cmap(norm(value))  # Get corresponding color from colormap
-                plt.text(j, i, f"{value}", color='black', ha='center', va='center', 
-                        fontsize=8, fontweight='bold')
-
-        plt.title(f'PSD peaks for a={a_range[a_val]}')  # Example title
-        plt.xlabel('Mean Delay')
-        plt.ylabel('Global Coupling')
-        plt.xticks(ticks=np.linspace(0, plot_list.shape[1] - 1, plot_list.shape[1]), 
-                labels=[f'{i:.0f}' for i in range(plot_list.shape[1])])
-        plt.yticks(ticks=tick_positions, labels=tick_labels)
-        a += 1
-    plt.tight_layout()
-    plt.savefig('graphs_mean/1_to_70/PSD_Peak_random_color.png')
-
-def add_scatter_fit(ax, x, y, color, label):
-    """Helper function to plot scatter points and fit line."""
-    ax.scatter(x, y, color=color, alpha=0.5, label=label, edgecolors='k')  # Scatter points
-    fit = np.poly1d(np.polyfit(x, y, deg=1))  # Quadratic fit
-    ax.plot(x, fit(x), color=color, linestyle='--', linewidth=2)  # Fit line
-
-
-def analyse_top_10():
-    E_Top10_gc_all_a = []
-    E_Top10_md_all_a = []
-    E_Top10_peak_alpha_all_a = []
-    E_score_all_a = []
-
-    Y_Top10_gc_all_a = []
-    Y_Top10_md_all_a = []
-    Y_Top10_peak_alpha_all_a = []
-    Y_score_all_a = []
-    for a_val in range(len(a_range)):
-        E_Top10_gc=[]
-        E_Top10_md=[]
-        E_Top10_peak_alpha=[]
-        E_Top10_score=[]
-
-        Y_Top10_gc=[]
-        Y_Top10_md=[]
-        Y_Top10_peak_alpha=[]
-        Y_Top10_score=[]
-
-        E_top_list = np.zeros((len(gc), len(md)))
-        Y_top_list = np.zeros((len(gc), len(md)))
-        for i in range(len(gc)):
-            for j in range(len(md)):
-                if True: #7 < all_psd_peak[a_val][i][j] < 13:
-                    E_top_list[i][j] = all_mean_corr_E[a_val][i][j]
-                    Y_top_list[i][j] = all_mean_corr_Y[a_val][i][j]
-        
-        # Get top 10 scores and their indices
-        flat_indices = np.argsort(E_top_list, axis=None)[-30:]
-        top_scores = E_top_list.flatten()[flat_indices]
-        top_scores = top_scores[::-1]
-        flat_indices= flat_indices[::-1]   ### INDICES MUST MATCH SCORES
-        E_top_indices = np.unravel_index(flat_indices, E_top_list.shape)   
-
-        top10=0
-        for score, i, j in zip(top_scores, E_top_indices[0], E_top_indices[1]):
-            if 7 < all_psd_peak[a_val][i][j] < 13:
-                if top10==10:
-                    break
-                else:
-                    E_Top10_gc.append(gc[i])
-                    E_Top10_md.append(md[j])
-                    E_Top10_peak_alpha.append(all_psd_peak[a_val][i][j])
-                    E_Top10_score.append(score)
-                    top10+=1
-        E_Top10_gc_all_a.append(E_Top10_gc)
-        E_Top10_md_all_a.append(E_Top10_md)
-        E_Top10_peak_alpha_all_a.append(E_Top10_peak_alpha)
-        E_score_all_a.append(E_Top10_score)
-
-        
-        flat_indices = np.argsort(Y_top_list, axis=None)[-30:]
-        top_scores = Y_top_list.flatten()[flat_indices]
-        top_scores = top_scores[::-1]
-        flat_indices= flat_indices[::-1]   ### INDICES MUST MATCH SCORES
-        Y_top_indices = np.unravel_index(flat_indices, Y_top_list.shape)
-
-        top10=0
-        for score, i, j in zip(top_scores, Y_top_indices[0], Y_top_indices[1]):
-            if 7 < all_psd_peak[a_val][i][j] < 13:
-                if top10==10:
-                    break
-                else:
-                    Y_Top10_gc.append(gc[i])
-                    Y_Top10_md.append(md[j])
-                    Y_Top10_peak_alpha.append(all_psd_peak[a_val][i][j])
-                    Y_Top10_score.append(score)
-                    top10+=1
-        Y_Top10_gc_all_a.append(Y_Top10_gc)
-        Y_Top10_md_all_a.append(Y_Top10_md)
-        Y_Top10_peak_alpha_all_a.append(Y_Top10_peak_alpha)
-        Y_score_all_a.append(Y_Top10_score)
-        
-    E_Top10_gc_mean = np.mean(E_Top10_gc_all_a, axis=1)
-    E_Top10_md_mean = np.mean(E_Top10_md_all_a, axis=1)
-    E_Top10_peak_alpha_mean = np.mean(E_Top10_peak_alpha_all_a, axis=1)
-    E_Top10_score_mean = np.mean(E_score_all_a, axis=1)
-
-    E_Top10_gc_var = np.var(E_Top10_gc_all_a, axis=1)
-    E_Top10_md_var = np.var(E_Top10_md_all_a, axis=1)
-    E_Top10_peak_alpha_var = np.var(E_Top10_peak_alpha_all_a, axis=1)
-    E_Top10_score_var = np.var(E_score_all_a, axis=1)
-
-    Y_Top10_gc_mean = np.mean(Y_Top10_gc_all_a, axis=1)
-    Y_Top10_md_mean = np.mean(Y_Top10_md_all_a, axis=1)
-    Y_Top10_peak_alpha_mean = np.mean(Y_Top10_peak_alpha_all_a, axis=1)
-    Y_Top10_score_mean = np.mean(Y_score_all_a, axis=1)
-
-    Y_Top10_gc_var = np.var(Y_Top10_gc_all_a, axis=1)
-    Y_Top10_md_var = np.var(Y_Top10_md_all_a, axis=1)
-    Y_Top10_peak_alpha_var = np.var(Y_Top10_peak_alpha_all_a, axis=1)
-    Y_Top10_score_var = np.var(Y_score_all_a, axis=1)
-
-    #plot all 4 parameters in one graph    
-    fig, (ax1, ax5) = plt.subplots(1, 2, figsize=(16, 6), sharex=True)
-
-    # --- First Dataset (Elder) ---
-    ax1.set_xlabel("a_range")
-    ax1.set_ylabel("GC Mean", color='b')
-    ax1.plot(a_range, E_Top10_gc_mean, color='b', label="GC Mean")
-    ax1.fill_between(a_range, E_Top10_gc_mean - np.sqrt(E_Top10_gc_var), E_Top10_gc_mean + np.sqrt(E_Top10_gc_var), color='b', alpha=0.2)
-    #add_scatter_fit(ax1, a_range, E_Top10_gc_mean, 'b', "GC Mean")
-    ax1.tick_params(axis='y', labelcolor='b')
-
-    ax2 = ax1.twinx()
-    ax2.set_ylabel("MD Mean", color='g')
-    ax2.plot(a_range, E_Top10_md_mean, color='g', label="MD Mean")
-    ax2.fill_between(a_range, E_Top10_md_mean - np.sqrt(E_Top10_md_var), E_Top10_md_mean + np.sqrt(E_Top10_md_var), color='g', alpha=0.2)
-    #add_scatter_fit(ax2, a_range, E_Top10_md_mean, 'g', "MD Mean")
-    ax2.tick_params(axis='y', labelcolor='g')
-
-    ax3 = ax1.twinx()
-    ax3.spines["right"].set_position(("outward", 60))
-    ax3.set_ylabel("Peak Alpha Mean", color='r')
-    ax3.plot(a_range, E_Top10_peak_alpha_mean, color='r', label="Peak Alpha Mean")
-    ax3.fill_between(a_range, E_Top10_peak_alpha_mean - np.sqrt(E_Top10_peak_alpha_var), E_Top10_peak_alpha_mean + np.sqrt(E_Top10_peak_alpha_var), color='r', alpha=0.2)
-    #add_scatter_fit(ax3, a_range, E_Top10_peak_alpha_mean, 'r', "Peak Alpha Mean")
-    ax3.tick_params(axis='y', labelcolor='r')
-
-    ax4 = ax1.twinx()
-    ax4.spines["right"].set_position(("outward", 120))
-    ax4.set_ylabel("Score", color='c')
-    ax4.plot(a_range, E_Top10_score_mean, color='c', label="Score")
-    ax4.fill_between(a_range, E_Top10_score_mean - np.sqrt(E_Top10_score_var), E_Top10_score_mean + np.sqrt(E_Top10_score_var), color='c', alpha=0.2)
-    #add_scatter_fit(ax4, a_range, E_Top10_score_mean, 'c', "Score")
-    ax4.tick_params(axis='y', labelcolor='c')
-
-    ax1.grid()
-    ax1.set_title("Elder Group: Multi-Axis Analysis with Fits")
-
-    # --- Second Dataset (Youth) ---
-    ax5.set_xlabel("a_range")
-    ax5.set_ylabel("GC Mean", color='b')
-    ax5.plot(a_range, Y_Top10_gc_mean, color='b', label="GC Mean")
-    ax5.fill_between(a_range, Y_Top10_gc_mean - np.sqrt(Y_Top10_gc_var), Y_Top10_gc_mean + np.sqrt(Y_Top10_gc_var), color='b', alpha=0.2)
-    #add_scatter_fit(ax5, a_range, Y_Top10_gc_mean, 'b', "GC Mean")
-    ax5.tick_params(axis='y', labelcolor='b')
-
-    ax6 = ax5.twinx()
-    ax6.set_ylabel("MD Mean", color='g')
-    ax6.plot(a_range, Y_Top10_md_mean, color='g', label="MD Mean")
-    ax6.fill_between(a_range, Y_Top10_md_mean - np.sqrt(Y_Top10_md_var), Y_Top10_md_mean + np.sqrt(Y_Top10_md_var), color='g', alpha=0.2)
-    #add_scatter_fit(ax6, a_range, Y_Top10_md_mean, 'g', "MD Mean")
-    ax6.tick_params(axis='y', labelcolor='g')
-
-    ax7 = ax5.twinx()
-    ax7.spines["right"].set_position(("outward", 60))
-    ax7.set_ylabel("Peak Alpha Mean", color='r')
-    ax7.plot(a_range, Y_Top10_peak_alpha_mean, color='r', label="Peak Alpha Mean")
-    ax7.fill_between(a_range, Y_Top10_peak_alpha_mean - np.sqrt(Y_Top10_peak_alpha_var), Y_Top10_peak_alpha_mean + np.sqrt(Y_Top10_peak_alpha_var), color='r', alpha=0.2)
-    #add_scatter_fit(ax7, a_range, Y_Top10_peak_alpha_mean, 'r', "Peak Alpha Mean")
-    ax7.tick_params(axis='y', labelcolor='r')
-
-    ax8 = ax5.twinx()
-    ax8.spines["right"].set_position(("outward", 120))
-    ax8.set_ylabel("Score", color='c')
-    ax8.plot(a_range, Y_Top10_score_mean, color='c', label="Score")
-    ax8.fill_between(a_range, Y_Top10_score_mean - np.sqrt(Y_Top10_score_var), Y_Top10_score_mean + np.sqrt(Y_Top10_score_var), color='c', alpha=0.2)
-    #add_scatter_fit(ax8, a_range, Y_Top10_score_mean, 'c', "Score")
-    ax8.tick_params(axis='y', labelcolor='c')
-
-
-    ax5.grid()
-    ax5.set_title("Youth Group: Multi-Axis Analysis with Fits")
-
-    plt.tight_layout()
-    plt.savefig('graphs_mean/1_to_70/Top_10_parameters_with_error.png', dpi=300, bbox_inches="tight")
 
 def return_contour(a_val):
     step_x_low = []  # x-axis values (e.g., mean delay indices)
@@ -1086,7 +850,7 @@ def compute_psd(signals, fs, nfft_factor=5):
     return frequencies, psd_mean
 
 def plot_psd(max_freq=250):
-    filepath = f'/vol/specs07/BasDrost/SL_simulation/simulation_results/signal_C13.97_md6_a-4.00_dt0.1.npy'
+    filepath = f'simulation_results/signal_C13.97_md6_a-4.00_dt0.1.npy'
                 
     # Load and preprocess the signal
     sig            = np.load(filepath)
@@ -1122,7 +886,6 @@ contour = True
 #plot_SEM_corr(Filter, contour)
 #top_10_mean_corr()
 #top_10_mean_corr_all()
-#analyse_top_10()
 #plot_psd()
             
 
